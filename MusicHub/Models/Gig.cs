eab9173efc6cd425c250047e69f836e703cc3422
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-02-04 20:42:19 -0500

    started gig class

---
+++ MusicHub/Models/Gig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicHub.Models
+{
+    public class Gig
+    {
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Venue { get; set; }
+        public Genre Genre { get; set; }
+
+
+
+
+
+    }
+
+    public class Genre
+    {
+        public byte  Id { get; set; }
+        public string Name { get; set;  }
+
+
+    }
+}
