10935d6a1f486054e30f8b21fb9f20f5cb0207fd
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-02-04 21:19:44 -0500

    completed gig Model

--- MusicHub/Models/ApplicationDbContext.cs
+++ MusicHub/Models/ApplicationDbContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MusicHub.Models
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
     }
 }
