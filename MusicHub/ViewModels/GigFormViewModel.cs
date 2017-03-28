c44d90cf18f52dcf37c358712c1c66b495f73370
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-03-28 15:26:05 -0400

    cleaned up the date time format

--- MusicHub/ViewModels/GigFormViewModel.cs
+++ MusicHub/ViewModels/GigFormViewModel.cs
@@ -13,6 +13,8 @@ namespace MusicHub.ViewModels
         public string Date { get; set; }
         public string Time { get; set; }
         public byte Genre { get; set; }
+        public DateTime DateTime { get { return DateTime.Parse(string.Format("{0} {1}", Date, Time)); } }
         public IEnumerable<Genre> Genres { get; set; }
+        
     }
 }
